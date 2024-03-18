c3915b74e14e139b56cefff86ff4d4ec9670b199
FadeScripting <79726377+Fadewasheres@users.noreply.github.com>
2024-03-18 21:49:55 +0100

    Add files via upload

--- src/webpack/common/types/components.d.ts
+++ src/webpack/common/types/components.d.ts
@@ -35,8 +35,6 @@ export type TextProps = PropsWithChildren<HtmlHTMLAttributes<HTMLDivElement> & {
 
 export type Text = ComponentType<TextProps>;
 
-export type FocusLock = ComponentType<FocusLock>;
-
 export type FormTitle = ComponentType<HTMLProps<HTMLTitleElement> & PropsWithChildren<{
     /** default is h5 */
     tag?: Heading;
@@ -156,7 +154,7 @@ export type Switch = ComponentType<PropsWithChildren<{
 }>>;
 
 export type Timestamp = ComponentType<PropsWithChildren<{
-    timestamp: Moment;
+    timestamp: Date | Moment;
     isEdited?: boolean;
 
     className?: string;
@@ -454,4 +452,8 @@ export type Avatar = ComponentType<PropsWithChildren<{
 
     "aria-hidden"?: boolean;
     "aria-label"?: string;
+}>>;
+
+type FocusLock = ComponentType<PropsWithChildren<{
+    containerRef: RefObject<HTMLElement>;
 }>>;
